1f4f9b356658926e99dcdc476e7a9bad768326e8
Hamster5295 <hamster5295@163.com>
2024-04-09 19:10:18 +0800

    增加音频的分贝转换工具

---
+++ Runtime/Utils/AudioUtils.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class AudioUtils
+{
+    public static float ValueToDb(float value)
+        => Mathf.Log10(Mathf.Max(1e-4f, value)) * 20;
+
+    public static float DbToValue(float db)
+        => Mathf.Pow(10, db / 20);
+}
