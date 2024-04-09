76ebf55dfb61de09171bd53d35cb7b0c00844a11
Hamster5295 <hamster5295@163.com>
2024-04-09 19:11:07 +0800

    增加针对向量的方便拓展

---
+++ Runtime/Utils/VectorExtensions.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+namespace Bingyan
+{
+    /// <summary>
+    /// 允许直接对 <see cref="Vector2"/>, <see cref="Vector3"/> 与 <see cref="Color"/> 的分量进行修改<br/> 的拓展
+    /// </summary>
+    public static class VectorExtensions
+    {
+        public static Vector3 SetX(this Vector3 vec, float x)
+        {
+            vec.x = x;
+            return vec;
+        }
+
+        public static Vector3 SetY(this Vector3 vec, float y)
+        {
+            vec.y = y;
+            return vec;
+        }
+
+        public static Vector3 SetZ(this Vector3 vec, float z)
+        {
+            vec.z = z;
+            return vec;
+        }
+
+        public static Vector2 SetX(this Vector2 vec, float x)
+        {
+            vec.x = x;
+            return vec;
+        }
+
+        public static Vector2 SetY(this Vector2 vec, float y)
+        {
+            vec.y = y;
+            return vec;
+        }
+
+        public static Color SetR(this Color color, float r)
+        {
+            color.r = r;
+            return color;
+        }
+
+        public static Color SetG(this Color color, float g)
+        {
+            color.g = g;
+            return color;
+        }
+
+        public static Color SetB(this Color color, float b)
+        {
+            color.b = b;
+            return color;
+        }
+
+        public static Color SetA(this Color color, float a)
+        {
+            color.a = a;
+            return color;
+        }
+    }
+}
