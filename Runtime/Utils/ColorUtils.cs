a0b79779084c3f38f3462f8da027364b16edc6f8
Hamster5295 <hamster5295@163.com>
2024-11-19 10:06:44 +0800

    完善颜色工具的命名空间与注释

--- Runtime/Utils/ColorUtils.cs
+++ Runtime/Utils/ColorUtils.cs
@@ -1,11 +1,20 @@
 using System;
 using UnityEngine;
 
-public static class ColorUtils
+namespace Bingyan
 {
-    public static Color FromHex(string hex)
+    public static class ColorUtils
     {
-        if (ColorUtility.TryParseHtmlString(hex, out var color)) return color;
-        else throw new ArgumentException($"Invalid hex string: {hex}");
+        /// <summary>
+        /// 将标准的 HEX 颜色码转为 Color
+        /// </summary>
+        /// <param name="hex">Hex 码，格式为 #rgb, #rgba, #rrggbb 或 #rrggbbaa</param>
+        /// <returns>颜色</returns>
+        /// <exception cref="ArgumentException">当输入颜色码不合法时抛出异常</exception>
+        public static Color FromHex(string hex)
+        {
+            if (ColorUtility.TryParseHtmlString(hex, out var color)) return color;
+            else throw new ArgumentException($"Invalid hex string: {hex}");
+        }
     }
 }
