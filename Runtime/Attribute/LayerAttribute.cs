f99f3e4ab5309eefc0a4fa54319206d0ad953c2f
Hamster5295 <hamster5295@163.com>
2025-01-15 12:12:56 +0800

    feat: 修改 LayerAttribute 的注释

--- Runtime/Attribute/LayerAttribute.cs
+++ Runtime/Attribute/LayerAttribute.cs
@@ -4,8 +4,8 @@ using System;
 namespace Bingyan
 {
     /// <summary>
-    /// 用于指定一个string类型的属性只能填入场景名称的标签
-    /// <para>主要是为了将Inspector里注册委托事件的过程变得更加"策划友好"</para>
+    /// 指定一个 int 类型的变量只能填入某个层级的序号<br/>
+    /// 类似于 LayerMask, 但只能选择一个 Layer，且得到序号而非掩码
     /// </summary>
     [AttributeUsage(AttributeTargets.Field)]
     public class LayerAttribute : PropertyAttribute { }
