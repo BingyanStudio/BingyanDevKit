fdd1bd0fb1e9d03fd8328ee27635e8abf3c10909
Hamster5295 <hamster5295@163.com>
2025-02-25 12:12:35 +0800

    refactor: 为 ScriptableConfig 引入加载日志

--- Runtime/Config/ScriptableConfig.cs
+++ Runtime/Config/ScriptableConfig.cs
@@ -21,15 +21,20 @@ namespace Bingyan
 #if UNITY_EDITOR
                     if (all.Length == 0)
                     {
-                        Log.I("ScriptableConfig", "未找到已加载的资源，尝试从项目中加载...");
+                        Log.I("ScriptableConfig", $"未找到已加载的资源，搜索: t:{typeof(T).Name}");
                         var paths = AssetDatabase.FindAssets($"t:{typeof(T).Name}");
                         if (paths.Length > 0)
                             all = new T[] { AssetDatabase.LoadAssetAtPath<T>(paths[0]) };
+                        else Log.E("ScriptableConfig", "未找到资源");
                     }
 #endif
 
                     if (all.Length == 0) Log.E("ScriptableConfig", "未找到配置，请至少创建一个!");
-                    else instance = all[0];
+                    else
+                    {
+                        instance = all[0];
+                        Log.I("ScriptableConfig", $"加载 {instance.name}");
+                    }
                 }
                 return instance;
             }
